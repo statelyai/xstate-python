a1e83b1a5a23bccbf172ec37248f6350676b7c5c
Lucas Bourneuf <lucas@bourneuf.net>
2021-01-21 10:40:11 +0100

    move setup data into cfg, fix licence info

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
-
-requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
-
-setup(
-    name="package",
-    version="0.0.1",
-    author="Jenny Truong",
-    author_email="truong.a.jenny@gmail.com",
-    description="A package to convert your Jupyter Notebook",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/your_package/homepage/",
-    packages=find_packages(),
-    install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    ],
-)
+setup()
