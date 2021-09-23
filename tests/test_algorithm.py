770e9c26c807ac12f9ad36a84c5fc47d43d4af9d
Anthony Uphof <anthony@smartlife-labs.com>
2021-09-23 21:41:02 +0000

    test: config as javasrcipt snippet

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -1,6 +1,63 @@
+"""Tests for  algorithims
+
+These tests use either machines coded as python `dict` 
+or accept strings representing xstate javascript/typescript which are
+then converted to a python `dict` by the module `js2py`
+
+The following tests exist
+    * test_is_parallel_state - returns the column headers of the file
+    * test_is_not_parallel_state - the main function of the script
+"""
 from xstate.algorithm import is_parallel_state
 from xstate.machine import Machine
 
+
+def test_machine_config_translate():
+
+    xstate_js_config =    '''{
+                id: 'fan',
+                initial: 'off',
+                states: {
+                    off: {
+                    on: {
+                        POWER:'on.hist',
+                        H_POWER:'on.H',
+                        SWITCH:'on.first'
+                    }
+
+                    },
+                    on: {
+                    initial: 'first',
+                    states: {
+                        first: {
+                        on: {
+                            SWITCH:'second'
+                        }
+                        },
+                        second: {
+                        on: {
+                            SWITCH: 'third'
+                        }
+                        },
+                        third: {},
+                        H: {
+                        type: 'history'
+                        },
+                        hist: {
+                        type: 'history',
+                        history:'shallow'
+                        },
+                    }
+                    }
+                }
+        }
+        '''
+    # xstate_python_config=machine_config_translate(xstate_js_config)
+    # assert isinstance(xstate_python_config,dict)
+    try:
+        machine = Machine(xstate_js_config)
+    except Exception as e:
+        assert False, f"Machine config is not valid, Exception:{e}"
 
 def test_is_parallel_state():
     machine = Machine(
